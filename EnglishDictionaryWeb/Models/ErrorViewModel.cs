8f480e0b6e1b347bf77afeb96ffc55dc15502854
ecevik1 <erenaycevik@outlook.com.tr>
2025-06-26 15:53:38 +0300

    Initial project setup

---
+++ EnglishDictionaryWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnglishDictionaryWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
